42e226f342d761400728b63b53486966fb2e3fb5
Nugine <nugine@foxmail.com>
2020-08-18 14:34:54 +0800

    use unsafe reader

--- src/byte_read.rs
+++ src/byte_read.rs
@@ -1,4 +1,6 @@
 use std::io::{BufRead, Read};
+use std::ptr;
+use std::slice;
 
 pub trait ByteRead: Read {
     fn next_byte(&mut self) -> Option<u8>;
@@ -20,8 +22,8 @@ impl ByteRead for &'_ [u8] {
 pub struct ByteReader<R> {
     inner: R,
     buf: Box<[u8]>,
-    pos: usize,
-    cap: usize,
+    head: *const u8,
+    tail: *const u8,
 }
 
 impl<R: Read> ByteReader<R> {
@@ -29,69 +31,75 @@ impl<R: Read> ByteReader<R> {
         Self {
             inner: reader,
             buf: vec![0; capacity].into(),
-            pos: 0,
-            cap: 0,
+            head: ptr::null(),
+            tail: ptr::null(),
         }
     }
 
+    #[allow(clippy::missing_safety_doc)]
     pub unsafe fn from_raw(buf: *mut [u8], reader: R) -> Self {
         Self {
             inner: reader,
             buf: Box::from_raw(buf),
-            pos: 0,
-            cap: 0,
+            head: ptr::null(),
+            tail: ptr::null(),
         }
     }
 }
 
 impl<R: Read> Read for ByteReader<R> {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let mut inner_buf = self.fill_buf()?;
-        let nread = inner_buf.read(buf)?;
-        self.consume(nread);
-        Ok(nread)
+        self.inner.read(buf)
     }
 }
 
 impl<R: Read> BufRead for ByteReader<R> {
     fn fill_buf(&mut self) -> std::io::Result<&[u8]> {
-        if self.pos < self.cap {
-            Ok(&self.buf[self.pos..self.cap])
+        if self.head != self.tail {
+            let len = self.tail as usize - self.head as usize;
+            Ok(unsafe { slice::from_raw_parts(self.head, len) })
         } else {
             let nread = self.inner.read(&mut *self.buf)?;
             if nread == 0 {
+                self.head = ptr::null();
+                self.tail = ptr::null();
                 Ok(&[])
             } else {
-                self.pos = 0;
-                self.cap = nread;
-                Ok(&self.buf[..nread])
+                self.head = self.buf.as_ptr();
+                self.tail = unsafe { self.head.add(nread) };
+                Ok(unsafe { slice::from_raw_parts(self.head, nread) })
             }
         }
     }
 
     fn consume(&mut self, amt: usize) {
-        self.pos = self.pos.saturating_add(amt).min(self.cap);
+        self.head = (self.head as usize)
+            .saturating_add(amt)
+            .min(self.tail as usize) as *const u8;
     }
 }
 
 impl<R: Read> ByteRead for ByteReader<R> {
     #[inline(always)]
     fn next_byte(&mut self) -> Option<u8> {
-        debug_assert!(self.cap <= self.buf.len());
-        if self.pos < self.cap {
-            let byte = self.buf[self.pos];
-            self.pos += 1;
-            Some(byte)
+        if self.head != self.tail {
+            unsafe {
+                let byte = *self.head;
+                self.head = self.head.add(1);
+                Some(byte)
+            }
         } else {
             match self.inner.read(&mut *self.buf) {
                 Ok(nread) => {
                     if nread == 0 {
                         None
                     } else {
-                        let byte = self.buf[0];
-                        self.cap = nread;
-                        self.pos = 1;
-                        Some(byte)
+                        unsafe {
+                            let byte = *self.buf.as_ptr();
+                            self.head = self.buf.as_ptr().add(1);
+                            self.tail = self.buf.as_ptr().add(nread);
+                            Some(byte)
+                        }
                     }
                 }
                 Err(e) => panic!(e),
